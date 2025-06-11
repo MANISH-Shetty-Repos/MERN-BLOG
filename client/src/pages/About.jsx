5fd2bc652a590c7059692eee7812a75e0c20ee85
MANISH-Shetty-Repos <manishshettyoth9182@gmail.com>
2025-06-11 16:24:14 +0530

    final project developed with all integrated curd operation.and functionalities
    with a functionalities like profile management.

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,18 @@
+export default function About() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Creative Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+            "Welcome to Creative Blog's! We're passionate about creating a space where individuals can share their thoughts, ideas, and stories with the world. Whether you're a seasoned writer or just starting out, our platform offers a user-friendly experience to help you craft engaging content and connect with like-minded readers. Our mission is to inspire creativity, spark meaningful conversations, and cultivate a vibrant community of bloggers. Join us on this journey of expression and discovery!"
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
